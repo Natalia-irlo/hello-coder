2c216f2c5f648a1372a300b964e036633047b2e5
natalia <natiriarte5@gmail.com>
2023-01-11 14:13:03 +0100

    navbar

---
+++ Brétema/bretema/src/App.js
@@ -0,0 +1,13 @@
+import Home from './components/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Brétema </h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ Brétema/bretema/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <div>
+            <h2>Pendientes de metacrilato</h2>
+        </div>
+    );
+}
+
+export default Home
